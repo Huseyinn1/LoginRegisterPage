3b0b26571c765484ad0f767edc31fea2241cb8dd
Hüseyin Adıgüzel <ahuseyin235@gmail.com>
2023-03-26 00:37:22 +0300

    Add project files.

---
+++ LoginRegisterPage/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using LoginRegisterPage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginRegisterPage.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //login işlemleri
+            }
+            return View(model);
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+
+    }
+}
